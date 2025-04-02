7408bd40b771a681da652b4bf14634ba6f31c0fc
miguel garcia <miguel.gareva@gmail.com>
2025-04-02 13:26:27 +0200

    test: add unit test to ensure battles belong to two trainers

---
+++ tests/Unit/BattleTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Battle;
+use App\Models\Trainer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BattleTest extends TestCase{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function testBattleBelongsToTwoTrainers(){
+
+        $trainer1 = Trainer::factory()->create();
+        $trainer2 = Trainer::factory()->create();
+
+        $battle = Battle::factory()->create([
+            'trainer1_id' => $trainer1->id,
+            'trainer2_id' => $trainer2->id
+        ]);
+
+        $this->assertInstanceOf(Trainer::class, $battle->trainer1);
+        $this->assertInstanceOf(Trainer::class, $battle->trainer2);
+        $this->assertEquals($trainer1->id, $battle->trainer1->id);
+        $this->assertEquals($trainer2->id, $battle->trainer2->id);
+    }
+}
