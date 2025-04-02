89cec25651e49ae2fe9e9c337a0af175580572d5
miguel garcia <miguel.gareva@gmail.com>
2025-04-02 13:34:16 +0200

    test: add unit test to verify a battle has a winner

--- tests/Unit/BattleTest.php
+++ tests/Unit/BattleTest.php
@@ -27,4 +27,20 @@ class BattleTest extends TestCase{
         $this->assertEquals($trainer1->id, $battle->trainer1->id);
         $this->assertEquals($trainer2->id, $battle->trainer2->id);
     }
+
+    /** @test */
+    public function testBattleHasWinner(){
+
+        $trainer1 = Trainer::factory()->create();
+        $trainer2 = Trainer::factory()->create();
+
+        $battle = Battle::factory()->create([
+            'trainer1_id' => $trainer1->id,
+            'trainer2_id' => $trainer2->id,
+            'winner_id' => $trainer1->id
+        ]);
+
+        $this->assertInstanceOf(Trainer::class, $battle->winner);
+        $this->assertEquals($trainer1->id, $battle->winner->id);
+    }
 }
