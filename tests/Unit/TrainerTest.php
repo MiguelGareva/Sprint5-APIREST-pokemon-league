14cfd1d24df46e1a32463027496543b3f73a4f60
miguel garcia <miguel.gareva@gmail.com>
2025-04-02 12:37:56 +0200

    test: add Trainer Model second test

--- tests/Unit/TrainerTest.php
+++ tests/Unit/TrainerTest.php
@@ -13,13 +13,26 @@ class TrainerTest extends TestCase{
 
     use RefreshDatabase;
 
-     /** @test */
-     public function testTrainerBelongsToUser()
+    /** @test */
+    public function testTrainerBelongsToUser()
      {
-         $user = User::factory()->create();
-         $trainer = Trainer::factory()->create(['user_id' => $user->id]);
+        $user = User::factory()->create();
+        $trainer = Trainer::factory()->create(['user_id' => $user->id]);
          
-         $this->assertInstanceOf(User::class, $trainer->user);
-         $this->assertEquals($user->id, $trainer->user->id);
+        $this->assertInstanceOf(User::class, $trainer->user);
+        $this->assertEquals($user->id, $trainer->user->id);
      }
+
+    /** @test */
+    public function testTrainerCanHavePokemons(){
+
+        $trainer = Trainer::factory()->create();
+        $pokemon1 = Pokemon::factory()->create(['trainer_id' =>$trainer->id]);
+        $pokemon2 = POkemon::factory()->create(['trainer_id' =>$trainer->id]);
+
+        $this->assertInstanceOf(Pokemon::class, $trainer->pokemons->first());
+        $this->assertCount(2, $trainer->pokemons);
+     }
+    
+    
 }
