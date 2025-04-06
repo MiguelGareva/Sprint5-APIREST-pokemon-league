bcdb16351165c8144c91b33475b9613cca8367ac
miguel garcia <miguel.gareva@gmail.com>
2025-04-06 13:20:33 +0200

    feat: create Trainer factory for testing

---
+++ database/factories/TrainerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Trainer>
+ */
+class TrainerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => fake()->name(),
+            'points' => fake()->numberBetween(0, 1000),
+        ];
+    }
+}
