b0faa3699ed6eb6fd85ab83727086041702bd918
miguel garcia <miguel.gareva@gmail.com>
2025-04-06 12:40:50 +0200

    feat: add battles table migration

---
+++ database/migrations/2025_04_06_103216_create_battles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('battles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trainer1_id')->constrained('trainers')->onDelete('cascade');
+            $table->foreignId('trainer2_id')->constrained('trainers')->onDelete('cascade');
+            $table->foreignId('winner_id')->nullable()->constrained('trainers')->onDelete('set null');
+            $table->datetime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('battles');
+    }
+};
