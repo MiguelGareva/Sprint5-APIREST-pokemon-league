071f33b98d38dcb1b9568f266a3147b6806877f4
miguel garcia <miguel.gareva@gmail.com>
2025-04-17 15:19:13 +0200

    feat: update API routes for new endpoints

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,17 @@ use App\Http\Controllers\API\TrainerController;
 use App\Http\Controllers\API\PokemonController;
 use App\Http\Controllers\API\BattleController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group.
+|
+*/
+
 // Public authentication routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,46 +27,54 @@ Route::get('/trainers/ranking', [TrainerController::class, 'ranking']);
 
 // Protected routes that require authentication
 Route::middleware('auth:api')->group(function () {
-    // Routes accessible to any authenticated user
+    // Authentication routes
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
-    // Update routes (accessible to trainers and admins)
-    Route::middleware('role:admin|trainer')->group(function () {
+    
+    // Routes accessible to any authenticated user
+    Route::get('/pokemons', [PokemonController::class, 'index']);
+    Route::get('/pokemons/available', [PokemonController::class, 'available']);
+    Route::get('/pokemons/{pokemon}', [PokemonController::class, 'show']);
+    Route::get('/battles', [BattleController::class, 'index']);
+    Route::get('/battles/{battle}', [BattleController::class, 'show']);
+    
+    // Trainer role routes
+    Route::middleware('role:trainer')->group(function () {
+        // Trainer-specific routes
+        Route::get('/trainers/{trainer}', [TrainerController::class, 'show'])->where('trainer', '[0-9]+');
+        Route::post('/battles', [BattleController::class, 'store']);
+        
+        // Pokemon assignment routes (trainers can modify their own pokemons)
         Route::post('/pokemons/{pokemon}/trainers/{trainer}', [PokemonController::class, 'assignToTrainer']);
         Route::delete('/pokemons/{pokemon}/trainers/{trainer}', [PokemonController::class, 'releaseFromTrainer']);
-    Route::middleware('role:admin|trainer')->group(function () {
-        Route::get('/battles', [BattleController::class, 'index']);
-        Route::get('/battles/{battle}', [BattleController::class, 'show']);
+        
+        // New routes for Step 6
+        Route::post('/battles/simulate', [BattleController::class, 'simulateBattle']);
+        Route::post('/pokemons/{pokemon}/transfer/{trainer}', [PokemonController::class, 'transferPokemon']);
     });
-    });
-    // Admin specific routes
+    
+    // Admin role routes
     Route::middleware('role:admin')->group(function () {
-        // Admin trainer routes
+        // Admin trainer management
         Route::get('/trainers', [TrainerController::class, 'index']);
         Route::post('/trainers', [TrainerController::class, 'store']);
         Route::put('/trainers/{trainer}', [TrainerController::class, 'update']);
         Route::delete('/trainers/{trainer}', [TrainerController::class, 'destroy']);
-        // Admin pokemon routes
+        
+        // Admin pokemon management
         Route::post('/pokemons', [PokemonController::class, 'store']);
         Route::put('/pokemons/{pokemon}', [PokemonController::class, 'update']);
         Route::delete('/pokemons/{pokemon}', [PokemonController::class, 'destroy']);
-        // Routes only for admins to delete battles
-        Route::middleware('role:admin')->group(function () {
-            Route::delete('/battles/{battle}', [BattleController::class, 'destroy']);
-        });
-    });
-    // Trainer specific routes
-    Route::middleware('role:trainer')->group(function () {
-        Route::get('/trainers/{trainer}', [TrainerController::class, 'show']);
-     // Trainer Pokemon routes (any authenticated user can view)
-    Route::get('/pokemons', [PokemonController::class, 'index']);
-    Route::get('/pokemons/available', [PokemonController::class, 'available']);
-    Route::get('/pokemons/{pokemon}', [PokemonController::class, 'show']);
-    // Routes only for trainers to create battles
-    Route::middleware('role:trainer')->group(function () {
-        Route::post('/battles', [BattleController::class, 'store']);
-    });
+        
+        // Admin battle management
+        Route::delete('/battles/{battle}', [BattleController::class, 'destroy']);
+        
+        // New admin routes for Step 6
+        Route::post('/trainers/{trainer}/points', [TrainerController::class, 'updatePoints']);
     });
     
-    
+    // Additional ranking routes (Step 6)
+    Route::get('/trainers/top/{count?}', [TrainerController::class, 'topTrainers']);
+    Route::get('/trainers/{trainer}/similar/{range?}', [TrainerController::class, 'similarTrainers']);
+    Route::get('/trainers/stats/monthly', [TrainerController::class, 'monthlyStats']);
 });
